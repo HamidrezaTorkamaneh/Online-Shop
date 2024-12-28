a814c514600a752ff2533eb5538723f1e9f6aeda
unknown <hr.torkamaneh@gmail.com>
2024-12-28 07:02:10 -0800

    finished bottom navigation bar

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -5,11 +5,19 @@ class Assets {
   static const String fontsYekanBakhBold = 'assets/fonts/YekanBakh-Bold.ttf';
   static const String fontsYekanBakhLight = 'assets/fonts/YekanBakh-Light.ttf';
   static const String fontsYekanBakhMedium = 'assets/fonts/YekanBakh-Medium.ttf';
+  static const String imagesIconActiveBasket = 'assets/images/icon_active_basket.svg';
+  static const String imagesIconActiveCategory = 'assets/images/icon_active_category.svg';
   static const String imagesIconActiveFav = 'assets/images/icon_active_fav.svg';
+  static const String imagesIconActiveHome = 'assets/images/icon_active_home.svg';
+  static const String imagesIconActiveUser = 'assets/images/icon_active_user.svg';
   static const String imagesIconApple = 'assets/images/icon_apple.svg';
+  static const String imagesIconBasket = 'assets/images/icon_basket.svg';
+  static const String imagesIconCategory = 'assets/images/icon_category.svg';
+  static const String imagesIconHome = 'assets/images/icon_home.svg';
   static const String imagesIconLeftArrowCircle = 'assets/images/icon_left_arrow_circle.svg';
   static const String imagesIconRightArrowCircle = 'assets/images/icon_right_arrow_circle.svg';
   static const String imagesIconSearch = 'assets/images/icon_search.svg';
+  static const String imagesIconUser = 'assets/images/icon_user.svg';
   static const String imagesIphone = 'assets/images/iphone.png';
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,43 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:online_shop/screens/category_screen.dart';
 import 'package:online_shop/screens/main_screen.dart';
+import 'package:online_shop/widgets/Custom_icon.dart';
 import 'package:online_shop/widgets/banner_slider.dart';
 import 'package:online_shop/widgets/category_items.dart';
 import 'package:online_shop/widgets/custom_color.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedBottomNavigationIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        localizationsDelegates: const [
-          GlobalCupertinoLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          Locale("fa", "IR"),
-        ],
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          textTheme: TextTheme(
+      localizationsDelegates: const [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("fa", "IR"),
+      ],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: TextTheme(
             headline1: TextStyle(
               fontFamily: 'YB',
               fontSize: 10,
@@ -43,13 +52,127 @@ class MyApp extends StatelessWidget {
               fontFamily: 'YB',
               fontSize: 10,
               color: CustomColor.greyColor,
-            )
+            )),
+      ),
+      home: Scaffold(
+        backgroundColor: CustomColor.backGroundColor,
+        appBar: AppBar(),
+        body: IndexedStack(
+          index: selectedBottomNavigationIndex,
+          children: getScreens(),
+        ),
+        bottomNavigationBar: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+            child: BottomNavigationBar(
+              onTap: (int index) {
+                setState(() {
+                  selectedBottomNavigationIndex = index;
+                });
+              },
+              currentIndex: selectedBottomNavigationIndex,
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              selectedLabelStyle: TextStyle(
+                fontFamily: 'YB',
+                fontSize: 10,
+                color: CustomColor.blueColor,
+              ),
+              unselectedLabelStyle: TextStyle(
+                fontFamily: 'YB',
+                fontSize: 10,
+                color: Colors.black,
+              ),
+              selectedItemColor: CustomColor.blueColor,
+              unselectedItemColor: Colors.black,
+              items: [
+                BottomNavigationBarItem(
+                  icon: CustomIcon(icon: 'home', color: Colors.black, size: 25),
+                  activeIcon: Container(
+                    padding: EdgeInsets.only(bottom: 3),
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        color: CustomColor.blueColor,
+                        blurRadius: 20,
+                        spreadRadius: -7,
+                        offset: Offset(0.0, 13),
+                      )
+                    ]),
+                    child: CustomIcon(
+                        icon: 'active_home', color: CustomColor.blueColor, size: 25),
+                  ),
+                  label: 'خانه',
+                ),
+                BottomNavigationBarItem(
+                  icon: CustomIcon(
+                      icon: 'category', color: Colors.black, size: 25),
+                  activeIcon: Container(
+                    padding: EdgeInsets.only(bottom: 3),
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        color: CustomColor.blueColor,
+                        blurRadius: 20,
+                        spreadRadius: -7,
+                        offset: Offset(0.0, 13),
+                      )
+                    ]),
+                    child: CustomIcon(
+                        icon: 'active_category',
+                        color: CustomColor.blueColor,
+                        size: 25),
+                  ),
+                  label: 'دسته بندی',
+                ),
+                BottomNavigationBarItem(
+                  icon:
+                      CustomIcon(icon: 'basket', color: Colors.black, size: 25),
+                  activeIcon: Container(
+                    padding: EdgeInsets.only(bottom: 3),
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        color: CustomColor.blueColor,
+                        blurRadius: 20,
+                        spreadRadius: -7,
+                        offset: Offset(0.0, 13),
+                      )
+                    ]),
+                    child: CustomIcon(
+                        icon: 'active_basket', color: CustomColor.blueColor, size: 25),
+                  ),
+                  label: 'سبد خرید',
+                ),
+                BottomNavigationBarItem(
+                  icon: CustomIcon(icon: 'user', color: Colors.black, size: 25),
+                  activeIcon: Container(
+                    padding: EdgeInsets.only(bottom: 3),
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        color: CustomColor.blueColor,
+                        blurRadius: 20,
+                        spreadRadius: -7,
+                        offset: Offset(0.0, 13),
+                      )
+                    ]),
+                    child: CustomIcon(
+                        icon: 'active_user', color: CustomColor.blueColor, size: 25),
+                  ),
+                  label: 'حساب کاربری',
+                ),
+              ],
+            ),
           ),
         ),
-        home: Scaffold(
-          backgroundColor: CustomColor.backGroundColor,
-          body: Center(
-              child: CategoryScreen()),
-        ));
+      ),
+    );
   }
+}
+
+List<Widget> getScreens() {
+  return <Widget>[
+    MainScreen(),
+    CategoryScreen(),
+    MainScreen(),
+    CategoryScreen(),
+  ];
 }
